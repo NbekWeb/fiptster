c88e589173d656df1c86812fd23a572799ff6c64
NbekWeb <nbekdev@gmail.com>
2025-05-11 11:29:33 +0500

    add axios and store

---
+++ src/stores/auth.pinia.js
@@ -0,0 +1,47 @@
+import { defineStore } from "pinia";
+import { api } from "@/utils/api.js";
+import { message } from "ant-design-vue";
+import useCore from "./core.pinia";
+
+const useAuth = defineStore("auth", {
+  state: () => ({
+    user: {},
+  }),
+  actions: {
+    postLogin(data, callback) {
+      api({
+        url: "login/",
+        method: "POST",
+        data,
+      })
+        .then(({ data }) => {
+          localStorage.setItem("access_token", data.access_token);
+          callback();
+        })
+        .catch((error) => {
+          message.error("Что-то пошло не так!");
+        })
+        .finally(() => {});
+    },
+    getUser(callback) {
+      const core = useCore();
+      core.loadingUrl.add("user");
+      api({
+        url: "profile/",
+        method: "GET",
+      })
+        .then(({ data }) => {
+          this.user = data;
+          callback(data.id)
+        })
+        .catch((error) => {
+          message.error("Что-то пошло не так!");
+        })
+        .finally(() => {
+          core.loadingUrl.delete("user");
+        });
+    },
+  },
+});
+
+export default useAuth;
