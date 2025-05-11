8c38f7fbf5e5bf405704d82cd1ef7f19edecc699
NbekWeb <nbekdev@gmail.com>
2025-05-12 01:45:50 +0500

    some fic

--- src/App.vue
+++ src/App.vue
@@ -24,14 +24,14 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-  <div class="h-full min-h-screen w-full">
+  <div class="h-full min-h-dvh w-full">
     <div
       v-if="!verticalMode"
       class="fixed inset-0 bg-dark-200 font-medium text-2xl text-white flex items-center justify-center z-50 text-center p-4"
     >
       To continue using FIPT, please switch back to vertical mode.
     </div>
-    <div v-else class="w-full min-h-screen">
+    <div v-else class="w-full min-h-dvh">
       <RouterView />
     </div>
   </div>
