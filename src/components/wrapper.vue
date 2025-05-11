14cb15b0cb28763094a570fe92333c1af7a17749
NbekWeb <nbekdev@gmail.com>
2025-05-12 01:32:01 +0500

    add padding bottom to earn

--- src/components/wrapper.vue
+++ src/components/wrapper.vue
@@ -7,7 +7,7 @@
       <slot name="top" />
     </div>
     <div
-      class="border-t-2 border-blue-500 rounded-t-50 px-7.5 pt-7  flex bg-dark-200 relative h-[calc(100%-52px)] -mt-12 overflow-y-hidden"
+      class="border-t-2 border-blue-500 rounded-t-50 px-7.5 pt-7 pb-5  flex bg-dark-200 relative h-[calc(100%-52px)] -mt-12 overflow-y-hidden"
     >
       <slot name="main" />
     </div>
